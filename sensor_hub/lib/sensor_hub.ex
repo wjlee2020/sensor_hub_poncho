da1bccc6061c8a4ee1db97b065f72e587fd94f52
WJ Lee <70941958+wjlee2020@users.noreply.github.com>
2023-08-06 17:43:37 +0900

    init: start nerves w/ ssh + wifi connection

---
+++ sensor_hub/lib/sensor_hub.ex
@@ -0,0 +1,22 @@
+defmodule SensorHub do
+  @moduledoc """
+  Documentation for SensorHub.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SensorHub.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def goodbye do
+    :world
+  end
+end
